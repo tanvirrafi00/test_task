a9ae2e863a259881eecd07d8990e942298d42a2e
Tanvir Rafi <tanvirrafi148@gmail.com>
2025-01-31 00:09:30 +0600

    changed configuration

--- configs/chrome.cucumber.conf.js
+++ configs/chrome.cucumber.conf.js
@@ -1,24 +1,33 @@
 import { downloadDir, mainConfig } from "../framework/configs/main.wdio.conf.js";
 
 export const config = {
-    ...mainConfig,
-    ...{
-        framework: 'cucumber',
-        cucumberOpts: {
-            require: ['./test/step-definitions/**/*.js'],
-        },
-        specs: [
-            '../test/features/**/*.feature'
-        ],
-        capabilities: [
-            {
-                browserName: "chrome",
-                "goog:chromeOptions": {
-                    prefs: {
-                        "download.default_directory": downloadDir
-                    }
-                },
-            },
-        ],
+  ...mainConfig,
+  ...{
+    framework: "cucumber",
+    cucumberOpts: {
+      require: ["./test/step-definitions/**/*.js"],
     },
+    specs: ["../test/features/**/*.feature"],
+    capabilities: [
+      {
+        browserName: "chrome",
+        maxInstances: 2,
+        "goog:chromeOptions": {
+          prefs: {
+            "download.default_directory": downloadDir,
+          },
+        },
+      },
+
+      {
+        browserName: "firefox",
+        maxInstances: 1,
+        "moz:firefoxOptions": {
+          prefs: {
+            "download.default_directory": downloadDir,
+          },
+        },
+      },
+    ],
+  },
 };
